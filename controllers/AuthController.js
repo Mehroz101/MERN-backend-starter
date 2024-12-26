d09ef013be8281c3c700beef879592d7796ddfa2
edusoftdevelopment <edusoftdevelopment@gmail.com>
2024-12-26 17:15:31 +0500

    first commit

---
+++ controllers/AuthController.js
@@ -0,0 +1,73 @@
+// const User = require("../models/User"); // Use require for imports
+// const jwt = require("jsonwebtoken"); // Example of another require
+const User = require("../models/User");
+export const signup = async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, password, confirmPassword } = req.body;
+    if (password === confirmPassword) {
+      const isUserExits = await User.findOne({ username });
+      if (isUserExits) {
+        return res.status(400).send({
+          success: false,
+          message: "User already exists",
+        });
+      } else {
+        const user = new User({
+          username,
+          password,
+          role: "student",
+        });
+        await user.save();
+        res.status(201).send({
+          success: true,
+          message: "User successfully signed up",
+        });
+      }
+    } else {
+      res.status(400).send({
+        success: false,
+        message: "Password does not match",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (user) {
+      if (user.password === password) {
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY);
+
+        res.status(200).send({
+          success: true,
+          message: "User successfully logged in",
+          token,
+        });
+      } else {
+        res.status(400).send({
+          success: false,
+          message: "Invalid password",
+        });
+      }
+    } else {
+      res.status(400).send({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+export default { signup, login };
---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("swagger-jsdoc");
+const { connectDB } = require("./config/db.js");
+require("dotenv").config();
+
+const path = require("path");
+
+const AuthRouter = require("./routes/AuthRouter.js");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static(path.join(__dirname, "uploads")));
+
+// Swagger setup
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "FeedBack App API",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: process.env.API_URL,
+      },
+    ],
+  },
+  apis: ["./routes/**/*.js"], // Correct path for route files
+};
+
+const swaggerSpec = swaggerDocument(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/api/auth", AuthRouter); // Make sure this is correctly set up
+connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
